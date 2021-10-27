2a0124921d3d83231b602a44d53065632427135a
Kulwch <76691359+Kulwch@users.noreply.github.com>
2021-10-27 22:42:21 +0200

    Setting project's frontend structure. Vue3 + vite plugin used.

---
+++ my-vue-app/src/router/index.js
@@ -0,0 +1,73 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import Home from '../views/Home.vue'
+
+const routes =[
+    {
+    path: '/',
+    name: 'Home',
+    component: Home,
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: () => import('../views/Signup.vue')
+  },
+  {
+    path: '/logout',
+    name: 'Logout',
+    component: () => import('../views/Logout.vue'),
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/registered',
+    name: 'Registered',
+    component: () => import('../views/Registered.vue')
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    component: () => import('../views/Profile.vue'),
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/admin/',
+    name: 'Admin',
+    component: () => import('../views/Admin.vue'),
+    meta: {
+      requiresAuth: true
+    }
+  }
+    ]
+
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes 
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (!localStorage.getItem('userId')) {
+      next({ name: 'Login' })
+    } else {
+      next() 
+    }
+  } else {
+    next()
+  }
+})
+
+export default router
